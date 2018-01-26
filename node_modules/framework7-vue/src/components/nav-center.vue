160509fb55a5ef4e6091cd8ef1ce0a28fa3135a8
Meinolf Droste <meinolf.droste@mdwp.de>
2018-01-26 17:55:38 +0100

    changes

---
+++ node_modules/framework7-vue/src/components/nav-center.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="center" :class="{sliding:sliding}"><slot>{{title}}</slot></div>
+</template>
+<script>
+  export default {
+    props: {
+        sliding: Boolean,
+        title: String
+    }
+  }
+</script>
